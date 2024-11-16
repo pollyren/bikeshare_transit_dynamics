e6e3650b490419a3e244cf500c6e8bdf1b34eb86
Polly Ren <pollyren@uchicago.edu>
2024-11-16 12:10:49 -0600

    download station information

--- 1_setup/download_bikeshare_data.sh
+++ 1_setup/download_bikeshare_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bs_dir="../data/bikeshare/"
+bs_dir="../data/bikeshare"
 mkdir -p "$bs_dir/chicago" "$bs_dir/la" "$bs_dir/nyc"
 
 download_metro() {
@@ -54,9 +54,25 @@ download_bikeshare() {
     esac
 }
 
+download_station_info() {
+    declare -A station_info=(
+        ["chicago"]="https://gbfs.lyft.com/gbfs/1.1/chi/en/station_information.json"
+        ["nyc"]="https://gbfs.citibikenyc.com/gbfs/en/station_information.json"
+        ["la"]="https://gbfs.bcycle.com/bcycle_lametro/station_information.json"
+    )
+
+    for city in "${!station_info[@]}"; do
+        url="${station_info[$city]}"
+        dir="$bs_dir/$city"
+        wget -P "$dir" "$url"
+        echo "downloaded station information for $city"
+    done
+}
+
 download_bikeshare "divvy"
 download_bikeshare "metro"
 download_bikeshare "citi"
+download_station_info
 
 for zip_file in $bs_dir/*/*.zip; do
     unzip -o "$zip_file" -d "$(dirname "$zip_file")"
