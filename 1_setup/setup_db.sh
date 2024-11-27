3349d4972d56e7ac8b3252629c594591aa1ffe82
Polly Ren <pollyren@uchicago.edu>
2024-11-26 18:02:13 -0600

    rename files for bikeshare setup

--- 1_setup/setup_db.sh
+++ 1_setup/setup_db.sh
@@ -9,7 +9,9 @@ if pg_isready -q && ! psql -lqt | cut -d \| -f 1 | grep -qw "$DB_NAME"; then
 fi
 
 psql -d $DB_NAME -c "CREATE EXTENSION IF NOT EXISTS postgis;"
-psql -d $DB_NAME -f create_schema.sql --set ON_ERROR_STOP=on
+
+# create bikeshare tables
+psql -d $DB_NAME -f create_bikeshare_schema.sql --set ON_ERROR_STOP=on
 
 # load bikeshare data into raw tables
 for file in ../data/bikeshare/chicago/*.csv; do
@@ -24,5 +26,13 @@ for file in ../data/bikeshare/la/*.csv; do
     cat $file | psql -d $DB_NAME -c "\COPY metro_trips_raw FROM stdin WITH CSV HEADER;"
 done
 
-# load data into final tables
-psql -d $DB_NAME -f populate_tables.sql --set ON_ERROR_STOP=on
+# load bikeshare data into final tables
+psql -d $DB_NAME -f populate_bikeshare_tables.sql --set ON_ERROR_STOP=on
+
+# load station information data
+python3 setup_station_info.py
+
+# create GTFS tables
+
+
+# load GTFS data
