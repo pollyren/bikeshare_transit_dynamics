8d22bdd4a44d2b83f167668db0f02888d852235f
Polly Ren <pollyren@uchicago.edu>
2025-03-11 22:32:53 -0500

    support weights matrix in spreg ols computation

--- 4_regressionAnalysis/spreg/ols.py
+++ 4_regressionAnalysis/spreg/ols.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from spreg import OLS
 from statsmodels.stats.outliers_influence import variance_inflation_factor
+import geopandas as gpd
+import libpysal
+import sys
+from collections import defaultdict
 
 VIF_THRESHOLD = 10
 
@@ -30,8 +34,24 @@ def remove_collinear_variables(df: pd.DataFrame):
     return df
 
 
-def run_spatial_ols(df: pd.DataFrame, df_name, outfile, ax_mi, ax_ms):
-    X = df.select_dtypes(include=['number']).dropna()
+def create_weights(df, shapefile):
+    gdf = gpd.read_file(shapefile)
+
+    gdf["GEOID"] = gdf["GEOID"].astype(str).str.zfill(11)
+    df.index = df.index.astype(str).str.zfill(11)
+    gdf_subset = gdf[gdf["GEOID"].isin(df.index.astype(str))]
+    gdf_subset = gdf_subset.to_crs("EPSG:5070") # use Conus Albers (metres instead of degrees)
+
+    w = libpysal.weights.DistanceBand.from_dataframe(gdf_subset, threshold=1600) # 1 mile threshold
+    w.transform = "r" # row standardise
+
+    return w
+
+
+def run_spatial_ols(df: pd.DataFrame, df_name, weights, outfile, ax_mi, ax_ms):
+    spatial = weights is not None
+
+    X = df
 
     mi_y = X["mi_count"] * 100 / X["num_trips"]
     ms_y = X["ms_count"] * 100 / X["num_trips"]
@@ -53,8 +73,10 @@ def run_spatial_ols(df: pd.DataFrame, df_name, outfile, ax_mi, ax_ms):
     X_np = X_std.to_numpy()
 
     # run spreg.OLS
-    mi_model = OLS(y_mi, X_np, name_y="mi_prop", name_x=X_std.columns.tolist())
-    ms_model = OLS(y_ms, X_np, name_y="ms_prop", name_x=X_std.columns.tolist())
+    mi_model = OLS(y_mi, X_np, name_y="mi_prop", name_x=X_std.columns.tolist(),
+                   w=weights, spat_diag=spatial, moran=spatial)
+    ms_model = OLS(y_ms, X_np, name_y="ms_prop", name_x=X_std.columns.tolist(),
+                   w=weights, spat_diag=spatial, moran=spatial)
 
     mi_coefs = pd.DataFrame({
         "Feature": X_std.columns,
@@ -70,17 +92,18 @@ def run_spatial_ols(df: pd.DataFrame, df_name, outfile, ax_mi, ax_ms):
         "Upper": ms_model.vm.diagonal() ** 0.5 * 1.96 + ms_model.betas.flatten()
     })
 
-    with open(outfile, "a") as f:
-        f.write(f"\nmodal integration, {df_name}:\n")
-        f.write(str(mi_model.summary))
+    if not weights: # skip VIF data for OLS with weights bc redundant
+        with open(outfile, "a") as f:
+            f.write(f"\nmodal integration, {df_name}:\n")
+            f.write(str(mi_model.summary))
 
-        f.write(f"\n\n\nmodal substitution, {df_name}:\n")
-        f.write(str(ms_model.summary))
+            f.write(f"\n\n\nmodal substitution, {df_name}:\n")
+            f.write(str(ms_model.summary))
 
-        vif_final = calculate_vif(X_filtered)
-        f.write("\nvif:\n")
-        f.write(vif_final.to_string(index=False))
-        f.write("\n-----\n")
+            vif_final = calculate_vif(X_filtered)
+            f.write("\nvif:\n")
+            f.write(vif_final.to_string(index=False))
+            f.write("\n-----\n")
 
     ax_mi.errorbar(mi_coefs["Coefficient"], mi_coefs["Feature"],
                    xerr=[mi_coefs["Coefficient"] - mi_coefs["Lower"], mi_coefs["Upper"] - mi_coefs["Coefficient"]],
@@ -100,41 +123,67 @@ def run_spatial_ols(df: pd.DataFrame, df_name, outfile, ax_mi, ax_ms):
 
 
 def main():
-    df_start_names = ["divvy_start", "citi_start", "metro_start"]
-    df_end_names = ["divvy_end", "citi_end", "metro_end"]
+    df_start_names = {
+        "divvy_start": "../../data/shapefiles/chicago/tl_2023_17_tract.shp",
+        "citi_start": "../../data/shapefiles/nyc/tl_2023_36_tract.shp",
+        "metro_start": "../../data/shapefiles/la/tl_2023_06_tract.shp"
+    }
+    df_end_names = {
+        "divvy_end": "../../data/shapefiles/chicago/tl_2023_17_tract.shp",
+        "citi_end": "../../data/shapefiles/nyc/tl_2023_36_tract.shp",
+        "metro_end": "../../data/shapefiles/la/tl_2023_06_tract.shp"
+    }
+
+    spatial = "--weights" in sys.argv
+    weights = defaultdict(lambda: None)
 
     dfs = {}
-    for df_name in df_start_names + df_end_names:
+    for df_name in list(df_start_names.keys()) + list(df_end_names.keys()):
         dfs[df_name] = pd.read_csv(f"../../data/final/{df_name}.csv", header=0, index_col=0)
         dfs[df_name]["mi_count"] = dfs[df_name][["mi_flm_count", "mi_fm_count", "mi_lm_count"]].sum(axis=1)
         dfs[df_name]["num_trips"] = dfs[df_name][["mi_count", "ms_count", "none_count"]].sum(axis=1)
+        dfs[df_name] = dfs[df_name].select_dtypes(include=['number']).dropna()
 
-    with open("output/spreg_ols_results_start.txt", "w"):
+        if spatial:
+            weights[df_name] = create_weights(dfs[df_name], (df_start_names | df_end_names)[df_name])
+
+
+    prefix = "spreg_ols" if not spatial else "spreg_olsw"
+
+    with open(f"output/{prefix}_results_start.txt", "w"):
         pass
-    with open("output/spreg_ols_results_end.txt", "w"):
+    with open(f"output/{prefix}_results_end.txt", "w"):
         pass
 
     num_rows_start = len(df_start_names)
     fig_start, axes_start = plt.subplots(num_rows_start, 2, figsize=(10, 5 * num_rows_start))
-    fig_start.suptitle("spreg OLS regression coefficients, trip origin", fontsize=16, y=0.99)
+    fig_start.suptitle(
+        f"spreg OLS regression coefficients{" (with weights)" if spatial else ""}, trip origin",
+        fontsize=16, y=0.99
+    )
     axes_start = axes_start.flatten()
 
     for i, df_name in enumerate(df_start_names):
-        run_spatial_ols(dfs[df_name], df_name, "output/spreg_ols_results_start.txt", axes_start[i * 2], axes_start[i * 2 + 1])
+        run_spatial_ols(dfs[df_name], df_name, weights[df_name],
+                        f"output/{prefix}_results_start.txt", axes_start[i * 2], axes_start[i * 2 + 1])
 
     plt.tight_layout()
-    plt.savefig("output/spreg_ols_coefs_start.png", bbox_inches="tight")
+    plt.savefig(f"output/{prefix}_coefs_start.png", bbox_inches="tight")
 
     num_rows_end = len(df_end_names)
     fig_end, axes_end = plt.subplots(num_rows_end, 2, figsize=(10, 5 * num_rows_end))
-    fig_end.suptitle("spreg OLS regression coefficients, trip destination", fontsize=16, y=0.99)
+    fig_end.suptitle(
+        f"spreg OLS regression coefficients{" (with weights)" if spatial else ""}, trip destination",
+        fontsize=16, y=0.99
+    )
     axes_end = axes_end.flatten()
 
     for i, df_name in enumerate(df_end_names):
-        run_spatial_ols(dfs[df_name], df_name, "output/spreg_ols_results_end.txt", axes_end[i * 2], axes_end[i * 2 + 1])
+        run_spatial_ols(dfs[df_name], df_name, weights[df_name],
+                        f"output/{prefix}_results_end.txt", axes_end[i * 2], axes_end[i * 2 + 1])
 
     plt.tight_layout()
-    plt.savefig("output/spreg_ols_coefs_end.png", bbox_inches="tight")
+    plt.savefig(f"output/{prefix}_coefs_end.png", bbox_inches="tight")
 
 if __name__ == "__main__":
     main()
